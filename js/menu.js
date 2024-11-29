7efbab047e3032223eab7e9d950c1849df2f75aa
Enrique36247 <enriqueperezgarcia@alumno.ieselrincon.es>
2024-11-29 19:17:22 +0000

    ns

---
+++ js/menu.js
@@ -0,0 +1,4 @@
+document.getElementById('menuToggle').addEventListener('click', function () {
+  var menu = document.getElementById('menu');
+  menu.style.display = (menu.style.display === 'block') ? 'none' : 'block';
+});
